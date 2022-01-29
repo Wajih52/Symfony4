3ce9d97c7750856079e45a28c24b3df84ac33b1e
Rjeb wajih <wajih.rjeb@esprit.tn>
2022-01-29 17:52:22 +0100

    seance 29-01-2022

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     */
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+    public function () {
+        return $this->render('student/listEtudiant.html.twig',array(tabStudent''))
+
+    }
+}
---
+++ src/Entity/Student.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StudentRepository::class)
+ */
+class Student
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $ref;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $Enabled;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getEnabled(): ?bool
+    {
+        return $this->Enabled;
+    }
+
+    public function setEnabled(bool $Enabled): self
+    {
+        $this->Enabled = $Enabled;
+
+        return $this;
+    }
+}
